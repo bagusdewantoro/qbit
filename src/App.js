5e13eefcbaa16a3ea2395285f3ed480f4f980c1c
bagusdewantoro <bagusdewantoro86@gmail.com>
2022-04-16 02:54:50 +0700

    router & redux implementation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import { useState } from 'react';
 
-function App() {
+import Main from './pages/Main';
+import Home from './pages/Home';
+import About from './pages/About';
+import Posting from './pages/Posting';
+import Login from './pages/Login';
+
+const App = () => {
+  const [login, setLogin] = useState(localStorage.getItem('auth-token'))
+  const handleLogout = () => {
+    delete localStorage['auth-token'];
+    setLogin(null)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Main login={login} handleLogout={handleLogout} />}>
+          <Route index element={<Home />} />
+          <Route path='about' element={<About />} />
+          <Route path='posting' element={<Posting />} />
+          <Route path='Login' element={<Login login={login} setLogin={setLogin} />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
